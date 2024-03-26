586ccba48971534c12df45e1bc11086a343991ca
Maxim Mukhamedov <113615768+Maxim0Mukhamedov@users.noreply.github.com>
2024-03-26 22:02:45 +0300

    Add files via upload

---
+++ adm6.py
@@ -0,0 +1,29 @@
+# Мухамедов Максим, БПМ-22-1 
+
+import random
+
+def random_permutation(string):
+  """
+  Функция генерирует случайную перестановку по введенной строке.
+
+  Args:
+    string: Строка, для которой генерируется перестановка.
+
+  Returns:
+    Случайная перестановка.
+  """
+  characters = list(string)
+
+  for i in range(len(characters)):
+    # Выбираем случайный индекс j в диапазоне [i, len(characters) - 1].
+    j = random.randint(i, len(characters) - 1)
+
+    # Меняем местами элементы characters[i] и characters[j].
+    characters[i], characters[j] = characters[j], characters[i]
+
+  return ''.join(characters)
+
+# Пример использования.
+string = input()
+random_permutation = random_permutation(string)
+print(random_permutation)
